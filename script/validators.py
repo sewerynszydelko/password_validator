9144633160db2c8479a370c40d0d6c0dade821cc
sewerynszydelko <seweryn494a@gmail.com>
2024-03-07 18:08:30 +0100

    Added lenght validaotr class , abstract Validaotr, ErrorValidation

---
+++ script/validators.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from requests import get
+
+
+class ValidationError(Exception):
+    pass
+
+
+class Validator(ABC):
+    """ Validator interface
+    """
+    @abstractmethod
+    def __init__(self, text):
+        pass
+
+    @abstractmethod
+    def is_valid(self, text):
+        pass
+
+
+class LenghtValidator(Validator):
+    """ Lenght Validaotr
+    """
+
+    def __init__(self, text):
+        """ Initalize validaotr lenght"""
+        self.text = text
+
+    def is_valid(self, lenght=8) -> bool:
+        """ Valid if given text is long enought
+        Args:
+            lenght (int, optional): Can be adjust . Defaults to 8.
+        Raises:
+            ValidationError: Riaise when text is shorter than set lenght
+        Returns:
+            bool : True or raise error
+        """
+        if len(self.text) == lenght:
+            return True
+
+        raise ValidationError("Text is too short")
+
+
+class PasswordValidator(Validatorl):
+
+    def __init__(self,password) -> None:
+        self.password = password
+
+    def is_valid(self):
+        pass
