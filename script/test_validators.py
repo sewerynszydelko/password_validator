557f82a13a386d007bb541f64e56084ed39cb9ed
sewerynszydelko <seweryn494a@gmail.com>
2024-03-07 18:52:01 +0100

    Added test for validators: Punctatuin,Number,UpperCaseChar

--- script/test_validators.py
+++ script/test_validators.py
@@ -1,8 +1,13 @@
-from validators import LenghtValidator, ValidationError, HasLowerLetterValidator
+from validators import (LenghtValidator,
+                        ValidationError,
+                        HasLowerCharactersValidator,
+                        HasUpperCharactersValidator,
+                        HasNumberValidator,
+                        HasPunctuationValidator)
 import pytest
 
 
-def test_lenght_validator_positive():
+def test_if_lenght_validator_positive():
     # given
     test_validator = LenghtValidator("abcdefgh")
 
@@ -13,7 +18,7 @@ def test_lenght_validator_positive():
     assert result is True
 
 
-def test_lenght_validator_negative():
+def test_if_lenght_validator_negative():
     # given
     test_validator = LenghtValidator("abc")
 
@@ -25,9 +30,9 @@ def test_lenght_validator_negative():
         assert "Text is too short" in str(error.value)
 
 
-def test_text_has_lower_letters_validaotr_possitive():
+def test_if_text_has_lower_character_validaotr_possitive():
     # given
-    validator = HasLowerLetterValidator("abCdeFG")
+    validator = HasLowerCharactersValidator("abCdeFG")
 
     # when
     result = validator.is_valid()
@@ -36,13 +41,79 @@ def test_text_has_lower_letters_validaotr_possitive():
     assert result is True
 
 
-def test_text_has_lower_letters_validaotr_negative():
+def test_if_text_has_lower_characters_validaotr_negative():
     # given
-    validator = HasLowerLetterValidator("ABCADA")
+    validator = HasLowerCharactersValidator("ABCADA")
 
     # when
     with pytest.raises(ValidationError) as error:
         validator.is_valid()
 
-        # then
+    # then
         assert "Text has no lower letters" in str(error.value)
+
+
+def test_if_text_has_upper_case_characters_validator_positive():
+    # given
+    validator = HasUpperCharactersValidator("abcdEfgg")
+
+    # when
+    result = validator.is_valid()
+
+    # then
+    assert result is True
+
+
+def test__if_text_has_upper_case_characters_validator_negative():
+    # given
+    validator = HasUpperCharactersValidator("abcd")
+
+    # when
+    with pytest.raises(ValidationError) as error:
+        validator.is_valid()
+
+    # then
+        assert "Text has no Upper case letters" in str(error.value)
+
+
+def test_if_text_have_number_validator_positive():
+    # given
+    validator = HasNumberValidator("ab2cdww9w")
+
+    # when
+    result = validator.is_valid()
+
+    # then
+    assert result is True
+
+
+def test_if_text_has_number_validator_negative():
+    # given
+    validator = HasNumberValidator("abcd")
+
+    # when
+    with pytest.raises(ValidationError) as error:
+        validator.is_valid()
+
+    # then
+        assert "Text has no numbers" in str(error.value)
+
+
+def test_if_text_has_punctuation_validator_positive():
+    # given
+    validator = HasPunctuationValidator("ab!c^d*%")
+
+    # when
+    result = validator.is_valid()
+
+    # then
+    assert result is True
+
+def test_if_text_has_punctuation_validator_negative():
+    # given
+    validator = HasPunctuationValidator("abcssda")
+
+    # when
+    with pytest.raises(ValidationError) as error:
+        validator.is_valid()
+        assert "Text has no punctiations like:'!@#$%^&*()_+'" in str(error.value)
