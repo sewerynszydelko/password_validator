23fd77072517ccd8d2e86e282e15a255b962439b
sewerynszydelko <seweryn494a@gmail.com>
2024-03-07 18:22:35 +0100

    Added test for lower case valid

--- script/test_validators.py
+++ script/test_validators.py
@@ -1,12 +1,14 @@
-from validators import LenghtValidator, ValidationError
+from validators import LenghtValidator, ValidationError, HasLowerLetterValidator
 import pytest
 
 
 def test_lenght_validator_positive():
     # given
     test_validator = LenghtValidator("abcdefgh")
+
     # when
     result = test_validator.is_valid()
+
     # then
     assert result is True
 
@@ -14,12 +16,33 @@ def test_lenght_validator_positive():
 def test_lenght_validator_negative():
     # given
     test_validator = LenghtValidator("abc")
+
     # when
-    """
     with pytest.raises(ValidationError) as error:
         test_validator.is_valid()
-        
-        assert "Text is too short" in str(error)"""
-    with pytest.raises(ValidationError) as error:
-        test_validator.is_valid()
+
+        # then
         assert "Text is too short" in str(error.value)
+
+
+def test_text_has_lower_letters_validaotr_possitive():
+    # given
+    validator = HasLowerLetterValidator("abCdeFG")
+
+    # when
+    result = validator.is_valid()
+
+    # then
+    assert result is True
+
+
+def test_text_has_lower_letters_validaotr_negative():
+    # given
+    validator = HasLowerLetterValidator("ABCADA")
+
+    # when
+    with pytest.raises(ValidationError) as error:
+        validator.is_valid()
+
+        # then
+        assert "Text has no lower letters" in str(error.value)
