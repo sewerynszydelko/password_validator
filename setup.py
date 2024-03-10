3e80694046689bbb66dd959fbd87c1d2a387b28c
sewerynszydelko <seweryn494a@gmail.com>
2024-03-10 12:08:22 +0100

    Added setup file for pacakge setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='password_validator',
+    version='0.1',
+    description='Colletion of password validators',
+    packages=['password_validator'],
+    install_requires=['requests']
+)
